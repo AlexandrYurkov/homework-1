f3c54c1364686c429e42926fa00f09b0014e07b7
AlexandrYurkov <alexandryurkov1@gmail.com>
2022-06-28 15:19:20 +0300

    clear

--- ex02.py
+++
@@ -1,6 +0,0 @@
-def int32_to_ip(int32):
-    one_byte = int(int32 / 16777216) % 256
-    two_byte = int(int32 / 65536) % 256
-    three_byte = int(int32 / 256) % 256
-    four_byte = int(int32) % 256
-    return '%(one_byte)s.%(two_byte)s.%(three_byte)s.%(four_byte)s' % locals()
--- ex03.py
+++
@@ -1,6 +0,0 @@
-def zeros(n):
-    result = 0
-    while n > 0:
-        n /= 5
-        result += (int(n))
-    return result
